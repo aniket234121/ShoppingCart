667e8088cea0265c3d498c6988861660b40c1adc
aniket234121 <aniketchauhan329@gmail.com>
2025-05-12 10:02:17 +0530

    added the Cart Service and Input validator

---
+++ src/main/java/com/shoppingCart/service/ShoppingCartService.java
@@ -0,0 +1,47 @@
+package com.shoppingCart.service;
+
+import com.shoppingCart.model.Product;
+import com.shoppingCart.*;
+import com.shoppingCart.repository.CartRepository;
+import java.util.Iterator;
+import java.util.List;
+
+public class ShoppingCartService {
+    private final CartRepository cartRepo;
+
+    public ShoppingCartService(CartRepository cartRepo) {
+        this.cartRepo = cartRepo;
+    }
+
+    public void addProduct(Product product) {
+        cartRepo.getCart().add(product);
+        cartRepo.saveCart();
+    }
+
+    public boolean removeProduct(String name) {
+        Iterator<Product> iterator = cartRepo.getCart().iterator();
+        while (iterator.hasNext()) {
+            Product p = iterator.next();
+            if (p.getName().equalsIgnoreCase(name)) {
+                iterator.remove();
+                cartRepo.saveCart();
+                return true;
+            }
+        }
+        return false;
+    }
+    public void clearCart() {
+        cartRepo.clearCart();
+    }
+
+    public List<Product> getCartItems() {
+        return cartRepo.getCart();
+    }
+
+    public double getCartAmount() {
+        return cartRepo.getCart().stream()
+                .mapToDouble(Product::getTotalPrice)
+                .sum();
+    }
+}
+
---
+++ src/main/java/com/shoppingCart/util/InputValidator.java
@@ -0,0 +1,77 @@
+package com.shoppingCart.util;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    public static String getValidName(Scanner scanner,String name) {
+        Matcher matcher;
+        do {
+            name=name.trim();
+            if (name.isEmpty()) {
+                System.out.println("Name cannot be empty.");
+            }
+            Pattern pt=Pattern.compile("^[A-Za-z][A-Za-z0-9]{2,19}$");
+            matcher=pt.matcher(name);
+            if(!matcher.matches()){
+                System.out.println("enter valid name");
+                name=scanner.nextLine();
+            }
+
+        } while (name.isEmpty()|| !matcher.matches());
+        return name;
+    }
+
+    public static double getValidPrice(Scanner scanner) {
+        double price = -1;
+        while (price <= 0) {
+
+            try {
+                price = Double.parseDouble(scanner.nextLine());
+                if (price <= 0) {
+                    System.out.println("Price must be a positive number.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+        return price;
+    }
+
+    public static int getValidQuantity(Scanner scanner) {
+        int qty = -1;
+        while (qty <= 0|| qty>100) {
+
+            try {
+                qty = Integer.parseInt(scanner.nextLine());
+                if (qty <= 0) {
+                    System.out.println("Quantity must be a positive integer.");
+                }
+                else if(qty>100)
+                {
+                    System.out.println("quantity must be between 1-100");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        return qty;
+    }
+
+    public static int getValidOption(Scanner scanner, int min, int max) {
+        int option = -1;
+        while (option < min || option > max) {
+            System.out.print("Please choose an option: ");
+            try {
+                option = Integer.parseInt(scanner.nextLine());
+                if (option < min || option > max) {
+                    System.out.println("Option must be between " + min + " and " + max + ".");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        }
+        return option;
+    }
+}
